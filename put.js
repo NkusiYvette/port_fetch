f1d029c29918cfd71a047d51f6a4b5b6e3d18cf0
UWIMPAYE Yvette <nkusiyvette1@gmail.com>
2023-12-21 13:19:28 +0200

    2nd

--- put.js
+++ put.js
@@ -1,18 +1,15 @@
-const btnEl = document.querySelector(".btn");
-
 fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) => response.json())
-.then((data) => {
-    for(i=0;i < data.length; i++)
-    console.log(data[i].username)
-});
-const userInfoDiv = document.getElementById('class').value = class{
-
-}
-
-
-// .then((data) => console.log(data[2].username)); 
-// .then((data) => console.log(data[3].username)); 
-// .then((data) => console.log(data[4].username)); 
-// .then((data) => console.log(data[5].username)); 
-// .then((data) => console.log(data[6].username)); 
+.then(res => {
+    if(!res.ok){
+        console.log('Problem');
+        return;
+    }
+    return res.json();
+})
+.then(data => {
+    for(let i =0; i< data.length; i++){
+        // console.log(data[i].name)
+        document.querySelector('ul').
+        insertAdjacentHTML("beforeend",`<li>${data[i].name}</li>`)
+    }
+})
